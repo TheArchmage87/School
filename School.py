239b0fa4412a3da0d384ff26f51eee85a144df8d
TheArchmage87 <144473122+TheArchmage87@users.noreply.github.com>
2024-03-06 10:52:18 -0500

    Add files via upload

--- School.py
+++ School.py
@@ -4,16 +4,18 @@ import random
 periodselect=input("What period? Respond with just the number.")
 #Replace class variables with period number
 #Represents kids in a  class
-P1=[]
+P1=["Joseph", "Ezra", "RJ", "Vikram", "Landon", "Ishaan", "Charlie", "Luke H", "Ava", "Peyton", "Luke L", "Bence", "Siya", "Ari Padmanabhan", "Kosta Patel", "Liam Probst", "Cameron Scott", "Rehan Shah", "Emily Shi", "Ahrian Vemuri", "Olivia Yu"]
 P2=["hi"]
 #Creates a second list so we can remove names from the list and still have the original list intact
 P12=P1
 P22=P2
 #Creates a repeting loop
 x=1
-while x==1:
-    #Creates a random seed(y) by choosing a random number from the range of the class list
-    if periodselect=="1":
+ #Creates a random seed(y) by choosing a random number from the range of the class list
+if periodselect=="1":
+    while x==1:
+        print(P12)
+        x=0 
         #Closes
         y=random.randint(1, len(P12))
         #Prints the item at that location but need to subtract one because Pythons index starts at 0 not 1
@@ -24,9 +26,18 @@ while x==1:
             #removes it from the list
             P12.remove(P12[y-1])
             print("Removed")
+            Roll=input("Do you want to roll again? Respond with Yes or No")
         #if no does nothing
         elif Remove=="No":
-            pass
+            Roll=input("Do you want to roll again? Respond with Yes or No")
         #otherwise says error
         else:
             print("Error")
+            input("Press Enter to exit")
+        if Roll=="Yes":
+            x=1
+        elif Roll=="No":
+            input("Press Enter to exit")
+        else:
+            print("Error")
+            input("Press Enter to exit")
